218d234054ba6c3811bbd1435d99dc3d30a5c2ef
rohankishorneog <rkn2601@gmail.com>
2024-10-19 13:02:24 +0530

    implemented and wired the hot questions on the rightsidebar to the database

--- components/shared/rightSideBar/RightSideBar.tsx
+++ components/shared/rightSideBar/RightSideBar.tsx
@@ -2,14 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "../RenderTag/RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
 
-const hotQuestions = [
-  { id: 1, question: "What is JavaScript's event loop?" },
-  { id: 2, question: "How does async/await work in JavaScript?" },
-  { id: 3, question: "What is closure in JavaScript?" },
-  { id: 4, question: "Explain prototypal inheritance in JavaScript." },
-  { id: 5, question: "What is the difference between var, let, and const?" },
-];
 const popularTags = [
   { id: 1, tag: "JavaScript", questions: 120 },
   { id: 2, tag: "React", questions: 80 },
@@ -18,7 +12,8 @@ const popularTags = [
   { id: 5, tag: "TypeScript", questions: 70 },
 ];
 
-const RightSideBar = () => {
+const RightSideBar = async () => {
+  const hotQuestions = await getHotQuestions();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       {" "}
@@ -27,11 +22,11 @@ const RightSideBar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions?.map((q) => (
             <Link
-              href={`/questions/${q.id}`}
+              href={`/question/${q.id}`}
               key={q.id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
-              <p className="body-medium text-dark500_light700">{q.question}</p>
+              <p className="body-medium text-dark500_light700">{q.title}</p>
               <Image
                 src={"/assets/icons/chevron-right.svg"}
                 alt="chevron-right"
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -243,3 +243,19 @@ export async function editQuestion(params: EditQuestionParams) {
     throw error;
   }
 }
+
+export async function getHotQuestions() {
+  try {
+    connectToDatabase();
+
+    const hotQuestions = await questionModel
+      .find({})
+      .sort({ views: -1, upvotes: -1 })
+      .limit(5);
+
+    return hotQuestions;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
