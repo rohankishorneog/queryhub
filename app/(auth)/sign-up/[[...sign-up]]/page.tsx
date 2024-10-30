19e196c500b5218d0debc1948904a507ec2cc457
rohankishorneog <rkn2601@gmail.com>
2024-10-30 12:52:32 +0530

    fixed signIn and signup page orientation

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="w-screen flex justify-center h-screen  items-center">
+      <SignUp />;
+    </div>
+  );
 }
